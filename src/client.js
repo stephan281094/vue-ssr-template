05d045632776a6a43b9a1b0ffe95494067bfd61e
stephan281094 <stephan281094@gmail.com>
2017-01-31 09:30:14 +0100

    Rewrite sw.js

--- src/client.js
+++ src/client.js
@@ -1,14 +1,10 @@
 import 'es6-promise/auto'
 import { app, store } from './app'
-import runtime from 'serviceworker-webpack-plugin/lib/runtime'
 
 store.replaceState(window.__INITIAL_STATE__)
 app.$mount('.o-app')
 
-// if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('/sw.js')
-// }
-
+// Register the service worker runtime
 if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-  runtime.register()
+  require('serviceworker-webpack-plugin/lib/runtime').register()
 }
--- src/sw.js
+++ src/sw.js
@@ -1,148 +1,42 @@
-const DEBUG = true
-
-/**
- * When the user navigates to your site,
- * the browser tries to redownload the script file that defined the service worker in the background.
- * If there is even a byte's difference in the service worker file compared to what it currently has,
- * it considers it 'new'.
- */
+const DIST_URL = global.location.origin + '/dist/'
+const CACHE_NAME = 'vue-ssr-template-' + (new Date()).toISOString()
 const { assets } = global.serviceWorkerOption
-const assetsToCache = assets.map((path) => {
-  return new URL(path, global.location).toString()
+const staticAssets = assets.map((path) => {
+  return new URL(path, DIST_URL).toString()
 })
 
-// When the service worker is first added to a computer.
-self.addEventListener('install', (event) => {
-  // Perform install steps.
-  if (DEBUG) {
-    console.log('[SW] Install event')
-  }
+const assetsToCache = [
+  ...staticAssets,
+  new URL('./', global.location).toString()
+]
 
-  // Add core website files to cache during serviceworker installation.
+self.addEventListener('install', (event) => {
   event.waitUntil(
     global.caches
       .open(CACHE_NAME)
       .then((cache) => {
         return cache.addAll(assetsToCache)
       })
-      .then(() => {
-        if (DEBUG) {
-          console.log('Cached assets: main', assetsToCache)
-        }
-      })
-      .catch((error) => {
-        console.error(error)
-        throw error
-      })
   )
 })
 
-// After the install event.
+// Delete previous caches and run idb migrations
 self.addEventListener('activate', (event) => {
-  if (DEBUG) {
-    console.log('[SW] Activate event')
-  }
+  console.log(assetsToCache)
 
-  // Clean the caches
   event.waitUntil(
     global.caches
       .keys()
       .then((cacheNames) => {
         return Promise.all(
-          cacheNames.map((cacheName) => {
-            // Delete the caches that are not the current one.
-            if (cacheName.indexOf(CACHE_NAME) === 0) {
-              return null
-            }
-
-            return global.caches.delete(cacheName)
-          })
+          cacheNames
+            .filter((cacheName) => {
+              return false // todo: add filter
+            })
+            .map((cacheName) => {
+              return global.caches.delete(cacheName)
+            })
         )
       })
   )
-})
-
-self.addEventListener('message', (event) => {
-  switch (event.data.action) {
-    case 'skipWaiting':
-      if (self.skipWaiting) {
-        self.skipWaiting()
-        self.clients.claim()
-      }
-      break
-    default:
-      break
-  }
-})
-
-self.addEventListener('fetch', (event) => {
-  const request = event.request
-
-  // Ignore not GET request.
-  if (request.method !== 'GET') {
-    if (DEBUG) {
-      console.log(`[SW] Ignore non GET request ${request.method}`)
-    }
-    return
-  }
-
-  const requestUrl = new URL(request.url)
-
-  const resource = global.caches.match(request)
-    .then((response) => {
-      if (DEBUG) {
-        console.log(`[SW] Fetching ${requestUrl.href}..`)
-      }
-
-      if (response) {
-        if (DEBUG) {
-          console.log(`[SW] fetch URL ${requestUrl.href} from cache`)
-        }
-
-        return response
-      }
-
-      // // Load and cache known assets.
-      // return global.fetch(request)
-      //   .then((responseNetwork) => {
-      //     if (!responseNetwork || !responseNetwork.ok) {
-      //       if (DEBUG) {
-      //         console.log(`[SW] URL [${
-      //           requestUrl.toString()}] wrong responseNetwork: ${
-      //           responseNetwork.status} ${responseNetwork.type}`)
-      //       }
-
-      //       return responseNetwork
-      //     }
-
-      //     if (DEBUG) {
-      //       console.log(`[SW] URL ${requestUrl.href} fetched`)
-      //     }
-
-      //     const responseCache = responseNetwork.clone()
-
-      //     global.caches
-      //       .open(CACHE_NAME)
-      //       .then((cache) => {
-      //         return cache.put(request, responseCache)
-      //       })
-      //       .then(() => {
-      //         if (DEBUG) {
-      //           console.log(`[SW] Cache asset: ${requestUrl.href}`)
-      //         }
-      //       })
-
-      //     return responseNetwork
-      //   })
-      //   .catch(() => {
-      //     // User is landing on our page.
-      //     if (event.request.mode === 'navigate') {
-      //       return global.caches.match('./')
-      //     }
-
-      //     return null
-      //   })
-    })
-
-  event.respondWith(resource)
 })
